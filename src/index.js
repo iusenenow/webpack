40df1a5ceb789cb1caddaf009748c52452b5ed94
Wei Zhang <iusenenow@gmail.com>
2020-08-06 11:13:41 +1000

    source map & oneOf

--- src/index.js
+++ src/index.js
@@ -33,3 +33,12 @@ const promise = new Promise(resolve => {
 })
 
 console.log(promise)
+
+if (module.hot) {
+  // module.hot为true，说明开启了HMR功能。 --> 让HMR功能代码生效
+  module.hot.accept('./print.js', () => {
+    // 方法会监听print.js文件的变化，一旦发生变化，其他默认不会重新打包构建。
+    // 会执行后面的回调函数。
+    print()
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,12 @@
 /*
   HMR: hot module replacement 热模块替换 / 模块热替换。
   作用：一个模块发生变化，只重新打包这一个模块，而不是打包所有（模块），极大提高构建速度。
+
+  样式文件：可以使用HMR功能：通过style-loader内部实现。
+  js文件：默认不适用HMR功能。--> 需要修改js代码，添加支持HMR功能的代码。
+    注意：HMR功能对js的处理，只能处理非入口js文件的其他文件。
+  html文件：默认不能使用HMR功能，同事会导致问题：html文件不能热更新了。
+    解决：entry改为array，把js文件和html文件同时引入：['./src/js/index.js', './src/index.html']
 */
 
 
@@ -32,8 +38,6 @@ module.exports = {
   },
   module: {
     rules: [
-      // loader配置：
-
       // js语法检查: eslint
       {
         /*
@@ -53,138 +57,144 @@ module.exports = {
         // 自动修复
         option: { fix: true }
       },
-
-      /*
-        js兼容性处理：babel-loader @babel/core @babel/preset-env
-        1. 基本js兼容性处理 --> @babel/preset-env
-        问题：只能转换基本语法，如Promise等；
-        2. 全部js兼容性处理 --> @babel/polyfill: import '@babel/polyfill'
-        问题：引入全部兼容性代码，体积太大；
-        3. 需要做兼容性处理：按需加载 --> core-js；
-      */
+      // oneOf: 以下loader只会匹配一个，注意不能有两个配置处理同一个文件
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        option: {
-          // 预设：指示babel做什么样的兼容性处理
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                // 按需加载
-                useBuiltIns: 'usage',
-                // 指定core-js版本
-                corejs: { version: 3 },
-                // 指定兼容性做到哪个版本浏览器
-                targets: {
-                  chrome: '60',
-                  firefox: '60',
-                  ie: '9',
-                  safari: '10',
-                  edge: '17'
-                }
-              }
-            ]
-          ]
-        }
-      },
-
-      // js压缩：将mode调成production模式，文件将自动压缩
-
-      // 处理css
-      {
-        test: /\.css$/i,
-        use: [
-          // 'style-loader',
-          // 取代style-loader。作用：提取js中的css成单独文件
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          // css兼容性处理：postcss --> postcss-loader postcss-preset-env
-          // 帮postcss找到package.json中browserslist里的配置，通过配置加载指定css兼容性形式
-          // "browserslist": {
-          // 开发环境 --> 设置node环境变量：process.env.NODE_ENV = development
-          //   "development": [
-          //     "last 1 chrome version",
-          //     "last 1 firefox version",
-          //     "last 1 safari version"
-          //   ],
-          // 生产环境：默认是看生产环境
-          //   "production": [
-          //     ">0.2%",
-          //     "not dead",
-          //     "not op_mini all"
-          //   ]
-          // }
-          // 使用loader的默认配置
-          // 'postcss-loader
-          // 修改loader的配置
+        oneOf: [
+          // loader配置：
+          /*
+            js兼容性处理：babel-loader @babel/core @babel/preset-env
+            1. 基本js兼容性处理 --> @babel/preset-env
+            问题：只能转换基本语法，如Promise等；
+            2. 全部js兼容性处理 --> @babel/polyfill: import '@babel/polyfill'
+            问题：引入全部兼容性代码，体积太大；
+            3. 需要做兼容性处理：按需加载 --> core-js；
+          */
           {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                // postcss插件
-                require('postcss-preset-env')()
-              ]
-            }
-          }
-
-        ]
-      },
-
-      // 处理less
-      {
-        test: /\.less$/i,
-        use: [
-          // 创建style标签，将样式放入'style-loader',
-          // 取代style-loader。作用：提取js中的css成单独文件
-          MiniCssExtractPlugin.loader,
-          // 将css文件整合到js文件中
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                // postcss插件
-                require('postcss-preset-env')()
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            option: {
+              // 预设：指示babel做什么样的兼容性处理
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    // 按需加载
+                    useBuiltIns: 'usage',
+                    // 指定core-js版本
+                    corejs: { version: 3 },
+                    // 指定兼容性做到哪个版本浏览器
+                    targets: {
+                      chrome: '60',
+                      firefox: '60',
+                      ie: '9',
+                      safari: '10',
+                      edge: '17'
+                    }
+                  }
+                ]
               ]
             }
           },
-          'less-loader'
+
+          // js压缩：将mode调成production模式，文件将自动压缩
+
+          // 处理css
+          {
+            test: /\.css$/i,
+            use: [
+              // 'style-loader',
+              // 取代style-loader。作用：提取js中的css成单独文件
+              MiniCssExtractPlugin.loader,
+              'css-loader',
+              // css兼容性处理：postcss --> postcss-loader postcss-preset-env
+              // 帮postcss找到package.json中browserslist里的配置，通过配置加载指定css兼容性形式
+              // "browserslist": {
+              // 开发环境 --> 设置node环境变量：process.env.NODE_ENV = development
+              //   "development": [
+              //     "last 1 chrome version",
+              //     "last 1 firefox version",
+              //     "last 1 safari version"
+              //   ],
+              // 生产环境：默认是看生产环境
+              //   "production": [
+              //     ">0.2%",
+              //     "not dead",
+              //     "not op_mini all"
+              //   ]
+              // }
+              // 使用loader的默认配置
+              // 'postcss-loader
+              // 修改loader的配置
+              {
+                loader: 'postcss-loader',
+                options: {
+                  ident: 'postcss',
+                  plugins: () => [
+                    // postcss插件
+                    require('postcss-preset-env')()
+                  ]
+                }
+              }
+
+            ]
+          },
+
+          // 处理less
+          {
+            test: /\.less$/i,
+            use: [
+              // 创建style标签，将样式放入'style-loader',
+              // 取代style-loader。作用：提取js中的css成单独文件
+              MiniCssExtractPlugin.loader,
+              // 将css文件整合到js文件中
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  ident: 'postcss',
+                  plugins: () => [
+                    // postcss插件
+                    require('postcss-preset-env')()
+                  ]
+                }
+              },
+              'less-loader'
+            ]
+          },
+
+          // 处理样式图片资源
+          {
+            test: /\.(png|jpg|gif)$/i,
+            loader: 'url-loader',
+            options: {
+              limit: 8 * 1024,
+              esModule: false,//关闭es6
+              name: '[hash:10].[ext]',
+              outputPath: 'imgs'
+            }
+          },
+
+          // 处理html中的img资源
+          {
+            test: /\.html$/,
+            loader: 'html-loader'
+          },
+
+          // 处理其他资源
+          {
+            exclude: /\.(css|js|html|json|less|jpg|png|gif)$/,
+            loader: 'file-loader',
+            options: {
+              name: '[hash10].[ext]',
+              outputPath: 'medias'
+            }
+          }
         ]
-      },
-
-      // 处理样式图片资源
-      {
-        test: /\.(png|jpg|gif)$/i,
-        loader: 'url-loader',
-        options: {
-          limit: 8 * 1024,
-          esModule: false,//关闭es6
-          name: '[hash:10].[ext]',
-          outputPath: 'imgs'
-        }
-      },
-
-      // 处理html中的img资源
-      {
-        test: /\.html$/,
-        loader: 'html-loader'
-      },
-
-      // 处理其他资源
-      {
-        exclude: /\.(css|js|html|json|less|jpg|png|gif)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[hash10].[ext]',
-          outputPath: 'medias'
-        }
       }
     ]
   },
+
   plugins: [
     // plugins配置
     new HtmlWebpackPlugin({
@@ -218,5 +228,50 @@ module.exports = {
     port: 3000,
     open: true,
     hot: true // 开启HMR功能。当修改了webpack配置，新配置想要生效，必须重启webpack。
-  }
+  },
+
+  /*
+    source-map: 一种提供源代码到构建后代码映射技术（如果构建后代码出错了，通过映射可以追踪源代码错误）:
+      [inline-|hidden-|eval-][nosources-][cheap-[module-]]source-map:
+      
+      source-map: 外部
+        错误代码准确信息 和 源代码的错误位置
+      inline-source-map: 内联
+        只生成一个内联source-map
+        错误代码准确信息 和 源代码的错误位置
+      hidden-source-map：外部
+        错误代码错误原因，但是没有错误位置，不能追踪源代码错误，只能提示到构建后代码的错误位置
+      eval-source-map：内联
+        每个文件都生成对应的source-map，都在eval
+        错误代码准确信息 和 源代码的错误位置
+      nosources-source-map：外部
+        错误代码准确信息，但是没有任何源代码信息
+      cheap-source-map：外部
+        错误代码准确信息 和 源代码的错误位置
+        只能精确到行，不能精确到列
+      cheap-module-source-map：外部
+        module会将loader的source map加入
+
+      内联和外部的区别：1. 外部生成了文件，内联没有；2.内联构建速度更快。
+
+      开发环境：速度快，调试更友好
+        速度（eval > inline > cheap > ...)
+          eval-cheap-sourse-map
+          eval-sourse-map
+        调试更友好
+          source-map
+          cheap-module-source-map
+          cheap-source-map
+
+      --> eval-source-map(react & vue默认) / eval-cheap-module-source-map
+
+      生产环境：源代码要不要隐藏？调试要不要更友好:
+        内联会让代码体积变大，所以生产环境不用内联
+        nosources-source-map（全部隐藏）
+        hidden-source-map（只隐藏源代码，不隐藏构建代码）
+
+        --> source-map / cheap-module-source-map
+  */
+  devtool: 'eval-source-map' // 开发模式
+  // devtool: 'source-map' 生产模式
 }
